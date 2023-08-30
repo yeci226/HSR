b49dcb479f012e81d87e1c25e30bcddb02197757
Yeci <88837499+yeci226@users.noreply.github.com>
2023-08-30 20:52:05 +0800

    Update getwarps.ps1

--- getwarps.ps1
+++ getwarps.ps1
@@ -3,13 +3,13 @@ Add-Type -AssemblyName System.Web
 $ProgressPreference = 'SilentlyContinue'
 
 #Find Game
-Write-Output "Finding game..."
+Write-Output "尋找遊戲中..."
 
 $appData = [Environment]::GetFolderPath("ApplicationData")
 $logPath = "$appData\..\LocalLow\Cognosphere\Star Rail\Player.log"
 $logContent = Get-Content $logPath #-First 1).replace("Loading player data from ", "").replace("/data.unity3d", "")
 $gamePath = ""
-$version = "2.14.0.0"
+$version = "2.15.0.0"
 
 for ($i = $logContent.Length - 1; $i -ge 0; $i--) {
     $line = $logContent[$logContent.Length -$i]
@@ -22,7 +22,7 @@ for ($i = $logContent.Length - 1; $i -ge 0; $i--) {
 
 if ($gamePath -ne "") {
     #Find Gacha Url
-    Write-Output "Finding Gacha Url..."
+    Write-Output "尋找躍遷網址..."
     Copy-Item -Path "$gamePath/webCaches/$version/Cache/Cache_Data/data_2" -Destination "$gamePath/webCaches/$version/Cache/Cache_Data/data_2_copy"
     $cacheData = Get-Content -Encoding UTF8 -Raw "$gamePath/webCaches/$version/Cache/Cache_Data/data_2_copy"
     Remove-Item -Path "$gamePath/webCaches/$version/Cache/Cache_Data/data_2_copy"
@@ -40,7 +40,7 @@ if ($gamePath -ne "") {
             if ($response.retcode -eq 0) {
                 Write-Output $url
                 Set-Clipboard -Value $url
-                Write-Output "Warp History Url has been saved to clipboard."
+                Write-Output "躍遷紀錄 URL 已保存到剪貼板。"
                 $foundUrl = "true"
                 return
             }
@@ -48,8 +48,8 @@ if ($gamePath -ne "") {
     }
 
     if ($foundUrl -eq "false") {
-        Write-Output "Unable to find Gacha Url. Please open warp history in-game."
+        Write-Output "無法找到躍遷網址。 請在遊戲中打開躍遷歷史記錄。"
     }
 } else {
-    Write-Output "Unable to find Game Path. Please try re-opening the game."
+    Write-Output "無法找到遊戲路徑。 請嘗試重新打開遊戲。"
 }
